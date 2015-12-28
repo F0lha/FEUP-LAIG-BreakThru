5de47c5bdf25ab0961dcb678a3ced76f2bf1aec5
F0lha <pedrovieiracastro@hotmail.com>
2015-12-28 08:38:39 +0000

    meh

--- reader/Board.js
+++ reader/Board.js
@@ -519,6 +519,8 @@ Board.prototype.getCoordArray = function(Char){
 
 }
 
+
+//funcoes para dar parsing ao rgb em hex
 function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
 function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
 function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
